fbb023f4c94cf54b2388e200dcd2cb9d892f5e7a
LizzieSevC <94926516+LizzieSevC@users.noreply.github.com>
2022-02-17 03:48:07 -0500

    Footer Update

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,85 @@
-<div>
-    <!-- An unexamined life is not worth living. - Socrates -->
-</div>
+
+        <style>
+
+            /*FOOTER*/
+            
+            /*DEVELOPERS LOGOS*/
+            .devLogo{
+                width: 6vw;
+                height: 6vw;
+            }
+
+        </style>
+
+
+
+{{-- FOOTER --}}
+
+        <footer class="text-center text-white" style="background-color: #13505B;">
+            <!-- Grid container -->
+
+            <div class="text-center text-light p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+                Contáctanos
+            </div>
+
+            <div class="container pt-4">
+                <!-- DEVELOPERS SOCIAL MEDIA -->
+                <section class="mb-4">
+                <!-- DANIEL GUTIERREZ -->
+                <a
+                    class="btn btn-link btn-floating btn-lg text-dark m-1"
+                    href="https://github.com/DanielgDiaz"
+                    role="button"
+                    data-mdb-ripple-color="dark"
+                    ><img class="devLogo" src="/Images/DanielGLogo.png"/>
+                </a>
+                
+                <!-- DANIEL PINZÓN -->
+                <a
+                    class="btn btn-link btn-floating btn-lg text-dark m-1"
+                    href="https://github.com/DanielKatt"
+                    role="button"
+                    data-mdb-ripple-color="dark"
+                    ><img class="devLogo" src="/Images/DanielPLogo.png"></i
+                ></a>
+            
+                <!-- LIZETTE SEVILLANO -->
+                <a
+                    class="btn btn-link btn-floating btn-lg text-dark m-1"
+                    href="https://github.com/LizzieSevC"
+                    role="button"
+                    data-mdb-ripple-color="dark"
+                    ><img class="devLogo" src="/Images/LizzieLogo.png"></i
+                ></a>
+            
+                <!-- HELEN DIDSBURY -->
+                <a
+                    class="btn btn-link btn-floating btn-lg text-dark m-1"
+                    href="#!"
+                    role="button"
+                    data-mdb-ripple-color="dark"
+                    ><i class="fab fa-instagram"></i
+                ></a>
+            
+                <!-- VALENTINA -->
+                <a
+                    class="btn btn-link btn-floating btn-lg text-dark m-1"
+                    href="#!"
+                    role="button"
+                    data-mdb-ripple-color="dark"
+                    ><i class="fab fa-linkedin"></i
+                ></a>
+                <!-- CARLOS FERRO -->
+                <a
+                    class="btn btn-link btn-floating btn-lg text-dark m-1"
+                    href="#!"
+                    role="button"
+                    data-mdb-ripple-color="dark"
+                    ><img class="devLogo" src="/Images/CarlosLogo.png"></i
+                ></a>
+                </section>
+
+                
+            </div>
+
+            </footer>
