e4ed841dc2ed6d909c88fec08293eeb76e13899e
DanielgDiaz <danielgdiaz92@gmail.com>
2022-02-17 09:10:49 +0100

    header footer & card:construction:

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+<header>
+    <x-header />
+</header>
+<body>
+    @section('content')
+    <div>
+        
+    </div>
+    @endsection
+
+    
+</body>
+
+<footer>
+    <x-footer />
+</footer>
